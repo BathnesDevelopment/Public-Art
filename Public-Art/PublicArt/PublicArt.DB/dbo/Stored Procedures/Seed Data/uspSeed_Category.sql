f269be87aa1f2f23fbc64a2a76f688b83804d777
Chris Pickford <chrispickford@gmail.com>
2016-01-19 11:24:15 +0000

    Added post deployment script and seed data for Category

---
+++ Public-Art/PublicArt/PublicArt.DB/dbo/Stored Procedures/Seed Data/uspSeed_Category.sql
@@ -0,0 +1,39 @@
+﻿/*
+	Name:			[dbo].[uspSeed_Category]
+	Author:			Chris Pickford
+	Created:		2016-01-19
+	Description:	Populates table with seed data to be executed post-deployment.
+*/
+CREATE PROCEDURE [dbo].[uspSeed_Category]
+AS
+
+SET NOCOUNT ON;
+
+MERGE INTO [dbo].[Category] AS [TARGET]
+USING ( VALUES 
+
+(N'Abstract'),
+(N'Church Art'),
+(N'Decorative'),
+(N'Drawing'),
+(N'Figurative'),
+(N'Functional'),
+(N'Mosaic'),
+(N'Mural'),
+(N'Other'),
+(N'Painting'),
+(N'Paving'),
+(N'Photography'),
+(N'Sculpture'),
+(N'Street furniture'),
+(N'Surrealist'),
+(N'Victorian')
+
+) AS [SOURCE] ([Description])
+ON [TARGET].[Description] = [SOURCE].[Description]
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT ([Description])
+	VALUES ([SOURCE].[Description]);
+
+RETURN 0;
+
--- Public-Art/PublicArt/PublicArt.DB/dbo/Tables/Category.sql
+++ Public-Art/PublicArt/PublicArt.DB/dbo/Tables/Category.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[Category]
 (
-	[CategoryId]		INT				NOT NULL		IDENTITY(1,1)
-,	[Description]		VARCHAR(50)		NOT NULL
+	[CategoryId]		INT					NOT NULL		IDENTITY(1,1)
+,	[Description]		NVARCHAR(100)		NOT NULL
 
-,	[rowguid]		UNIQUEIDENTIFIER	NOT NULL		ROWGUIDCOL		CONSTRAINT [DF_Category_rowguid] DEFAULT (NEWID())
-,	[ModifiedDate]	DATETIME2			NOT NULL						CONSTRAINT [DF_Category_ModifiedDate] DEFAULT (SYSDATETIME())
+,	[rowguid]			UNIQUEIDENTIFIER	NOT NULL		ROWGUIDCOL		CONSTRAINT [DF_Category_rowguid] DEFAULT (NEWID())
+,	[ModifiedDate]		DATETIME2			NOT NULL						CONSTRAINT [DF_Category_ModifiedDate] DEFAULT (SYSDATETIME())
 
 ,	CONSTRAINT [PK_Category_CategoryId] PRIMARY KEY CLUSTERED ([CategoryId])
 );
