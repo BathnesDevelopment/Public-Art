1d1e39aa28e9d25d162f9ba7d4b0c558b681bc87
Chris Pickford <chrispickford@gmail.com>
2016-01-19 10:54:08 +0000

    Added columns to Item table

--- Public-Art/PublicArt/PublicArt.DB/dbo/Tables/Item.sql
+++ Public-Art/PublicArt/PublicArt.DB/dbo/Tables/Item.sql
@@ -1,17 +1,42 @@
 ﻿CREATE TABLE [dbo].[Item]
 (
-	[ItemId]		INT				NOT NULL		IDENTITY(1,1)
-,	[Title]			[dbo].[Name]	NOT NULL
-,	[Reference]		VARCHAR(6)		NOT NULL
-,	[ItemDate]		DATE			NULL		-- TODO: Change this column to a FK referencing some kind of log table
+	[ItemId]					INT					NOT NULL		IDENTITY(1,1)
 
+,	[Title]						[dbo].[Name]		NOT NULL
+,	[Reference]					VARCHAR(6)			NOT NULL
+,	[Description]				NVARCHAR(2000)		NOT NULL
 
-,	[rowguid]		UNIQUEIDENTIFIER	NOT NULL		ROWGUIDCOL		CONSTRAINT [DF_Item_rowguid] DEFAULT (NEWID())
-,	[ModifiedDate]	DATETIME2			NOT NULL						CONSTRAINT [DF_Item_ModifiedDate] DEFAULT (SYSDATETIME())
+,	[Statement]					NVARCHAR(2000)		NULL
+,	[Material]					NVARCHAR(500)		NULL
+,	[Inscription]				NVARCHAR(500)		NULL
+,	[History]					NVARCHAR(2000)		NULL
+,	[Notes]						NVARCHAR(2000)		NULL
+
+,	[Height]					SMALLINT			NULL
+,	[Width]						SMALLINT			NULL
+,	[Depth]						SMALLINT			NULL
+,	[Diameter]					SMALLINT			NULL
+
+,	[SurfaceCondition]			NVARCHAR(1000)		NULL
+,	[StructuralCondition]		NVARCHAR(1000)		NULL
+
+,	[Address]					NVARCHAR(1000)		NULL
+,	[Location]					GEOGRAPHY			NULL
+
+,	[Archived]					BIT					NOT NULL						CONSTRAINT [DF_Item_Deleted] DEFAULT (0)
+
+,	[rowguid]					UNIQUEIDENTIFIER	NOT NULL		ROWGUIDCOL		CONSTRAINT [DF_Item_rowguid] DEFAULT (NEWID())
+,	[ModifiedDate]				DATETIME2			NOT NULL						CONSTRAINT [DF_Item_ModifiedDate] DEFAULT (SYSDATETIME())
 
 ,	CONSTRAINT [PK_ArtItem_ItemId] PRIMARY KEY CLUSTERED ([ItemId])
 );
 GO
 
-CREATE UNIQUE INDEX [IX_Item_Reference] ON [dbo].[Item] ([Reference])
+CREATE UNIQUE INDEX [UX_Item_Reference] ON [dbo].[Item] ([Reference])
+GO
+
+CREATE SPATIAL INDEX [SI_Item_Location] ON [dbo].[Item]([Location]) USING GEOGRAPHY_AUTO_GRID
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Item_Archived] ON [dbo].[Item]([Archived]);
 GO
