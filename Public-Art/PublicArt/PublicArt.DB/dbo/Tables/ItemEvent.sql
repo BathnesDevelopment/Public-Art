cdcde407293996b74714130e17cbe128c8dc5419
Chris Pickford <chrispickford@gmail.com>
2016-01-19 09:33:07 +0000

    Added ItemEvent table

---
+++ Public-Art/PublicArt/PublicArt.DB/dbo/Tables/ItemEvent.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ItemEvent]
+(
+	[ItemId]		INT					NOT NULL
+,	[EventId]		TINYINT				NOT NULL
+,	[EventDate]		DATE				NOT NULL
+,	[Description]	NVARCHAR(1000)		NOT NULL
+
+,	[rowguid]		UNIQUEIDENTIFIER	NOT NULL		ROWGUIDCOL		CONSTRAINT [DF_ItemEvent_rowguid] DEFAULT (NEWID())
+,	[ModifiedDate]	DATETIME2			NOT NULL						CONSTRAINT [DF_ItemEvent_ModifiedDate] DEFAULT (SYSDATETIME())
+
+,	CONSTRAINT [PK_ItemEvent_ItemId_EventId] PRIMARY KEY CLUSTERED ([ItemId], [EventId])
+,	CONSTRAINT [FK_ItemEvent_ItemId_Item] FOREIGN KEY ([ItemId]) REFERENCES [dbo].[Item]([ItemId])
+
+);
+GO
