5bf76df60829a6f38d6b46aad898980e633aad9c
Chris Pickford <chrispickford@gmail.com>
2016-01-21 14:19:36 +0000

    Added Add and Delete SPs for ImageFT

---
+++ Public-Art/PublicArt/PublicArt.DB/dbo/Stored Procedures/uspImage_Add.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[uspImage_Add] (
+     @filename NVARCHAR(255)
+    ,@filedata VARBINARY(MAX)
+    )
+AS
+DECLARE @docid UNIQUEIDENTIFIER;
+BEGIN
+    SET @docid = NEWID();
+    INSERT  INTO [dbo].[Image]
+            ([stream_id]
+            ,[file_stream]
+            ,[name]
+            )
+    VALUES
+            (@docid
+            ,@filedata
+            ,@filename
+            );
+    SELECT
+        [stream_id]
+    ,   [file_stream].[PathName]() AS [unc_path]
+    FROM
+        [dbo].[Image]
+    WHERE
+        [stream_id] = @docid;
+END;
---
+++ Public-Art/PublicArt/PublicArt.DB/dbo/Stored Procedures/uspImage_Delete.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[uspImage_Delete] (
+     @docId UNIQUEIDENTIFIER
+    )
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DELETE FROM
+        [dbo].[Image]
+    WHERE
+        [stream_id] = @docId;    
+END;
