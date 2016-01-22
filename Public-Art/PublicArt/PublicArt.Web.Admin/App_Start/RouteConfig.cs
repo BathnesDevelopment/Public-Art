8908b7268a5e8572c27cd24e9646972c4db60d75
Chris Pickford <chrispickford@gmail.com>
2016-01-22 13:07:43 +0000

    Enabled attribute routing

--- Public-Art/PublicArt/PublicArt.Web.Admin/App_Start/RouteConfig.cs
+++ Public-Art/PublicArt/PublicArt.Web.Admin/App_Start/RouteConfig.cs
@@ -13,11 +13,13 @@ namespace PublicArt.Web.Admin
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            );            
         }
     }
 }
