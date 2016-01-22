6dd7c22757117a2ab9b616559b1377f8fe7ebdfe
Chris Pickford <chrispickford@gmail.com>
2016-01-22 13:50:26 +0000

    Added ImageThumbnail table

---
+++ Public-Art/PublicArt/PublicArt.DB/dbo/Tables/ImageThumbnail.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ImageThumbnail] (
+    [stream_id]         UNIQUEIDENTIFIER                NOT NULL        ROWGUIDCOL
+,   [file_stream]       VARBINARY(MAX)  FILESTREAM      NOT NULL
+,   [ModifiedDate]      DATETIME2                       NOT NULL        CONSTRAINT [DF_ImageThumbnail_ModifiedDate] DEFAULT (SYSDATETIME())
+,   CONSTRAINT [PK_ImageThumbnail_stream_id] PRIMARY KEY CLUSTERED ([stream_id])
+,   CONSTRAINT [FK_ImageThumbnail_stream_id_ImageFT] FOREIGN KEY ([stream_id]) REFERENCES [dbo].[ImageFT]([stream_id])
+);
+GO
