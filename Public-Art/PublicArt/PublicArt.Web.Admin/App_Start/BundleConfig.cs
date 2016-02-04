90c81136abb7c3a4937886e91e682d1ab51c63e0
Chris Pickford <chrispickford@gmail.com>
2016-02-04 11:15:25 +0000

    Implemented bootstrap-toggle library

--- Public-Art/PublicArt/PublicArt.Web.Admin/App_Start/BundleConfig.cs
+++ Public-Art/PublicArt/PublicArt.Web.Admin/App_Start/BundleConfig.cs
@@ -9,33 +9,40 @@ namespace PublicArt.Web.Admin
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js"
+            ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*"
+            ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*"
+            ));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/respond.js"
+            ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/site.css"
+            ));
 
             bundles.Add(new ScriptBundle("~/bundles/thirdparty").Include(
                 "~/Scripts/DataTables/jquery.dataTables.min.js",
                 "~/Scripts/DataTables/dataTables.bootstrap.min.js",
+                "~/Scripts/bootstrap-toggle.min.js",
                 "~/Scripts/featherlight.min.js",
                 "~/Scripts/leaflet-0.7.3.min.js"
             ));
 
             bundles.Add(new StyleBundle("~/Content/thirdparty").Include(
                 "~/Content/DataTables/css/dataTables.bootstrap.min.css",
+                "~/Content/bootstrap-toggle.min.css",
                 "~/Content/featherlight.min.css",
                 "~/Content/leaflet.css"
             ));
