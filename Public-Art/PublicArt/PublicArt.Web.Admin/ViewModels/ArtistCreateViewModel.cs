f5db1ef50173e98ea0a7d752a2f9cc8c8c81a099
Chris Pickford <chrispickford@gmail.com>
2016-02-11 12:45:02 +0000

    Modified create pages

---
+++ Public-Art/PublicArt/PublicArt.Web.Admin/ViewModels/ArtistCreateViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PublicArt.Web.Admin.ViewModels
+{
+    public class ArtistCreateViewModel
+    {
+        [Display(Name = "Name")]
+        [Required]
+        [StringLength(300)]
+        public string Name { get; set; }
+
+        [Display(Name = "Biography")]
+        [StringLength(4000)]
+        [DataType(DataType.MultilineText)]
+        public string Biography { get; set; }
+
+        [Display(Name = "Website")]
+        [Url]
+        [StringLength(2083)]
+        [DataType(DataType.Url)]
+        public string WebsiteUrl { get; set; }
+
+        [Display(Name = "Start Year")]
+        [Range(1000, 2200)]
+        public short? StartYear { get; set; }
+
+        [Display(Name = "End Year")]
+        [Range(1000, 2200)]
+        public short? EndYear { get; set; }
+    }
+}
