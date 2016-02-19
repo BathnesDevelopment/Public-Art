d6111329867a5460839c1b2e47956a6bab3a4888
Chris Pickford <chrispickford@gmail.com>
2016-02-19 13:23:40 +0000

    Created basic home page

--- Public-Art/PublicArt/PublicArt.Web.Admin/Controllers/HomeController.cs
+++ Public-Art/PublicArt/PublicArt.Web.Admin/Controllers/HomeController.cs
@@ -1,11 +1,18 @@
-﻿using System.Web.Mvc;
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using PublicArt.DAL;
 
 namespace PublicArt.Web.Admin.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly PublicArtEntities _db = new PublicArtEntities();
+
+        public async Task<ActionResult> Index()
         {
+            ViewBag.ArtistCount = await _db.Artists.CountAsync();
+            ViewBag.ItemCount = await _db.Items.CountAsync();
             return View();
         }
     }
